7ab5c228a94eb2ad7cd305a4a33daef5fa0d94f1
SDPreetham <135338835+SDPreetham@users.noreply.github.com>
2023-08-05 14:39:54 +0000

    Site in making

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-console.log("Hello BBS")
+const state = {
+taskList: [],
+};
+//DOM
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector
+const htmlTaskContent = ({id,title,description,type,url}) =>`
+<div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
+<div class='card shadow task__card'>
+<div class='card-header d-flex gap-2 justify-content-end task__card__header'>
+<button type='button' class ='btn btn-outline-info mr-2'name=${id}><i class='fas fa-pencil-alt'></i></button>
+<button type='button' class ='btn btn-outline-danger mr-2'name=${id}><i class='fas fa-trash-alt'></i></button>
+</div>
+<div class='card-body'>
+${
+    url &&
+    `<img src=${url} alt='card image' class='card-img-top md-3 rounded-md'/>`
+}
+<h4 class='card-title'>${title}</h4>
+<p class='card-text text-muted'>${description}</p>
+<div class='tags d-flex flex-wrap'>
+<span class='badge text-white bg-primary m-1'>${type}</span>
+</div>
+</div>
+<div class='card-footer'> 
+<button type='button' class='btn btn-outline-primary float-end' data-bs-toggle='modal' data-bs-target='#showTask'>Open Task</button>
+</div>
+</div>
+</div>
+`
+const htmlModalContent = ({id,title,description,url}) =>{
+    const date = new Date(parseInt(id));
+    return `
+<div id=${id}>
+${
+    url &&
+    `<img src=${url} alt='card image' class='img-fluid rounded place__holder__image mb-3' />`
+}
+<strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>
+<h4 class='my-2'>${title}</h4>
+<p class='text-muted'>${description}</p>
+</div>    `
+};
+
+const updateLocalStorage = () =>{
+localStorage.setItem('task',JSON.stringify({
+tasks:state.taskList,
+}));
+};
+
+const loadInitialData =()=>{
+const localStorageCopy = JSON.parse(localStorage.task);
+if(localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+state.taskList.map((cardDate)=>{
+    taskContents.insertAdjacentHTML("beforeend",htmlTaskContent(cardDate));
+});
+
+};
+const postSubmit = ()=>{
+const id = `${Date.now()}`
+const input = {
+    url: document.getElementById('imageUrl').value,
+    title:document.getElementById('tasktitle').value,
+    description:document.getElementById('taskdesc').value,
+    type:document.getElementById('tags').value,
+};
+if(input.title==='' || input.description==='' || input.type===''){
+    return alert("Please fill out all the necessary fields");
+}
+taskContents.insertAdjacentHTML(
+    "beforeend",htmlTaskContent({...input,id})
+);
+state.taskList.push({...input,id});
+updateLocalStorage();
+}
